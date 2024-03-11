f6cec0c9710fb9a26711476c27ce7b4fab6e7a79
Aitor <aduqueagullo@gmail.com>
2024-03-11 20:28:47 +0100

    Agregados 4 tests para el metodo 'RegistroDia'

--- Tarea2UD6/src/es/iesmz/tests/DiasLluviaTest.java
+++ Tarea2UD6/src/es/iesmz/tests/DiasLluviaTest.java
@@ -1,7 +1,31 @@
 package es.iesmz.tests;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DiasLluviaTest {
-    
+    @Test
+    public void compruebaRegistroDiaNoDejaValoresMayoresDe12EnMes1() {
+        DiasLluvia d = new DiasLluvia();
+        assertEquals(false, d.registroDia(1,13,true));
+    }
+
+    @Test
+    public void compruebaRegistroDiaNoDejaValoresMayoresDe12EnMes2() {
+        DiasLluvia d = new DiasLluvia();
+        assertEquals(false, d.registroDia(1,13,true));
+    }
+
+    @Test
+    public void compruebaRegistroDiaNoDejaValoresMayoresDe32EnDia1() {
+        DiasLluvia d = new DiasLluvia();
+        assertEquals(false, d.registroDia(32,12,true));
+    }
+
+    @Test
+    public void compruebaRegistroDiaNoDejaValoresMayoresDe32EnDia2() {
+        DiasLluvia d = new DiasLluvia();
+        assertNotEquals(true, d.registroDia(32,12,true));
+    }
 }
