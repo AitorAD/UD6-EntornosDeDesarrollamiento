11a87c9574ca191b99666dee8f85a2db145de8d9
Aitor <aduqueagullo@gmail.com>
2024-03-11 21:05:03 +0100

    Agregados 3 tests para el metodo 'contarDiasLluviosos'

--- Tarea2UD6/src/es/iesmz/tests/DiasLluviaTest.java
+++ Tarea2UD6/src/es/iesmz/tests/DiasLluviaTest.java
@@ -49,4 +49,25 @@ class DiasLluviaTest {
         DiasLluvia d = new DiasLluvia();
         assertNotEquals(true, d.consultarDia(1,1));
     }
+
+    // Test metodo contarDiasLluviosos
+    @Test
+    public void compruebaContarDiasLluviososEs2SiHanLlovido2Dias() {
+        DiasLluvia d = new DiasLluvia();
+        d.registroDia(1,1,true);
+        d.registroDia(2,1,true);
+        assertEquals(2, d.contarDiasLluviosos());
+    }
+
+    @Test
+    public void compruebaContarDiasLluviososEs0SiHanLlovido0Dias() {
+        DiasLluvia d = new DiasLluvia();
+        assertEquals(0, d.contarDiasLluviosos());
+    }
+
+    @Test
+    public void compruebaContarDiasLluviososEs0SiHanLlovido2Dias() {
+        DiasLluvia d = new DiasLluvia();
+        assertNotEquals(2, d.contarDiasLluviosos());
+    }
 }
