05700d5d620f3ce1a6a3e690ecbc8edf836de397
Aitor <aduqueagullo@gmail.com>
2024-03-11 10:21:54 +0100

    Añadido segundo metodo a la Tarea 2

--- Tarea2UD6/src/es/iesmz/tests/DiasLluvia.java
+++ Tarea2UD6/src/es/iesmz/tests/DiasLluvia.java
@@ -1,11 +1,20 @@
 package es.iesmz.tests;
 
 public class DiasLluvia {
-    int[][] calendario = new int[12][31];
+    boolean[][] calendario = new boolean[12][31];
 
     boolean registroDia(int dia, int mes, boolean lluvia) {
         if (dia >= 1 && dia <= 31 && mes <= 1 && mes >= 12) {
-            return true;
+            calendario[mes][dia] = true;
+        }
+        return false;
+    }
+
+    boolean consultarDia(int dia, int mes) {
+        for (int i = 0; i < calendario.length; i++) {
+            for (int j = 0; j < calendario[i].length; j++) {
+                if (calendario[i][j]) return true;
+            }
         }
         return false;
     }
