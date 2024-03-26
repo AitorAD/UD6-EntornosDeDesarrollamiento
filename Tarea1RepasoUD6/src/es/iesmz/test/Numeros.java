826e8fc638de0230b82ec13c229465959732785f
Aitor <aduqueagullo@gmail.com>
2024-03-26 20:51:03 +0100

    Agregada Tarea 1 repaso

---
+++ Tarea1RepasoUD6/src/es/iesmz/test/Numeros.java
@@ -0,0 +1,15 @@
+package es.iesmz.test;
+
+public class Numeros {
+    public static int sonIguales(int a, int b, int c) {
+        int valor;
+        if (a == b && a == c) {
+            valor = 0;
+        } else if (a != b && b != c && c != a) {
+            valor = 2;
+        } else {
+            valor = 1;
+        }
+        return valor;
+    }
+}
